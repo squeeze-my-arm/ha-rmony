e45594c6920a82a9d22914c76ac23627ede68637
Lee SangIn <tescodanish@naver.com>
2023-08-09 13:45:15 +0900

    :sparkles: 컬럼 이동, 수정
    BoardColumn
    - board_column 테이블의 컬럼명 변경
    - update 메서드 추가
    
    Controller
    - 컬럼 수정 API 생성
    - deleteBoardColumn 구조 변경
      - Service부분에서 컬럼, 보드 받아오기
    
    Req
    - 필드명 변경 (columnName -> boardColumnName)
    - boardColumnOrder 필드 추가
    
    Res
    - boardColumnOrder 필드 추가
    
    Service, ServiceImpl
    - 주석 추가
    - updateBoardColumn 기능 구현
    - deleteBoardColumn 기능 개선
      - 보드와 컬럼 ServiceImpl에서 받아오기

--- src/main/java/com/harmony/boardColumn/BoardColumnController.java
+++ src/main/java/com/harmony/boardColumn/BoardColumnController.java
@@ -1,7 +1,5 @@
 package com.harmony.boardColumn;
 
-import com.harmony.board.Board;
-import com.harmony.board.BoardService;
 import com.harmony.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,29 +13,33 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class BoardColumnController {
 
-    private final BoardColumnRepository boardColumnRepository;
     private final BoardColumnServiceImpl boardColumnService;
-    private final BoardService boardService;
 
     // 컬럼 생성
     // 하이픈 사용 유무 체크
     @PostMapping("/board-columns")
-    public ResponseEntity<BoardColumnResponseDto> createPost(@RequestBody BoardColumnRequestDto boardColumnRequestDto,
+    public ResponseEntity<BoardColumnResponseDto> createBoardColumn(@RequestBody BoardColumnRequestDto boardColumnRequestDto,
                                                   @AuthenticationPrincipal UserDetailsImpl userDetails) {
         BoardColumnResponseDto boardColumn = boardColumnService.createBoardColumn(boardColumnRequestDto, userDetails.getUser());
         return ResponseEntity.status(201).body(boardColumn);
     }
 
+    // 컬럼 수정
+    @PutMapping("/board-columns/{columnId}")
+    public ResponseEntity<BoardColumnResponseDto> updateBoardColumn(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                                        @RequestBody BoardColumnRequestDto boardColumnRequestDto,
+                                                        @PathVariable Long columnId) {
+
+            BoardColumnResponseDto responseDto = boardColumnService.updateBoardColumn(columnId, boardColumnRequestDto, userDetails.getUser());
+            return ResponseEntity.ok().body(responseDto);
+    }
+
     // 컬럼 삭제
-    @DeleteMapping("/board-columns/{id}")
+    @DeleteMapping("/board-columns/{columnId}")
     public ResponseEntity<String> deleteBoardColumn(@AuthenticationPrincipal UserDetailsImpl userDetails,
-                                                    @PathVariable Long id) {
-            BoardColumn boardColumn = boardColumnService.findBoardColumn(id);
-//            log.info(boardColumn.getBoardColumnName());
-            Board board = boardService.findBoard(boardColumn.getBoard().getId());
-//            log.info(board.getBoardTitle());
-            boardColumnService.deleteBoardColumn(boardColumn, board);
-//            boardColumnRepository.deleteById(boardColumn.getId());
+                                                    @PathVariable Long columnId) {
+
+            boardColumnService.deleteBoardColumn(columnId);
             return ResponseEntity.ok().body("컬럼이 삭제 되었습니다.");
     }
 }
