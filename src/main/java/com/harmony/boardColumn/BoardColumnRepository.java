63f6448036069c826d0a1dc83d9f45ae1b355750
azuressu <martina0130@naver.com>
2023-08-12 13:45:21 +0900

    :memo: 카드 수정 기능 수정

--- src/main/java/com/harmony/boardColumn/BoardColumnRepository.java
+++ src/main/java/com/harmony/boardColumn/BoardColumnRepository.java
@@ -2,8 +2,12 @@ package com.harmony.boardColumn;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Arrays;
 import java.util.List;
 
 public interface BoardColumnRepository extends JpaRepository<BoardColumn, Long> {
     List<BoardColumn> findByBoardId(Long boardId);
+
+    List<BoardColumn> findAllByBoardIdOrderByBoardColumnOrder(Long boardId);
+
 }
---
+++ src/main/java/com/harmony/boardUser/BoardUserResponseDto.java
@@ -0,0 +1,14 @@
+package com.harmony.boardUser;
+
+import lombok.Getter;
+
+@Getter
+public class BoardUserResponseDto {
+
+    private String username;
+
+    public BoardUserResponseDto(BoardUser boardUser){
+        this.username = boardUser.getUsername();
+    }
+
+}
